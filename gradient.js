4cd9d5b9eaa0f5497ed0706a5035306ba6460dce
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-06-24 16:29:33 +0100

    Removed everything except questions

--- gradient.js
+++
@@ -1,7 +0,0 @@
-function gradient(object,event){
-    object.style.background = `radial-gradient(circle at ${(event.offsetX/1000)*100}%, darkgray,gray)`
-};
-
-function removeGradient(object){
-    object.style.background = "gray"
-};
--- setup.js
+++
@@ -1,3 +0,0 @@
-const supabaseUrl = 'https://hxfnilmbkorrzhhnohzp.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4Zm5pbG1ia29ycnpoaG5vaHpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc2MTEzNTcsImV4cCI6MjAwMzE4NzM1N30.1RD7ecSAK9RGQJdFKk7iOck2KA70xvygYYdjXmJfWcg'
-const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey)
