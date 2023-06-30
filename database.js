4582bf550413e3e57120f96bfc89dba29c7f47e4
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-06-30 18:01:16 +0100

    FauAdding 500 to fln has now been moved to a seperate file and now works if the leaderboard is accessed before the main script. It is not on the login page as the user is not logged in and RLS is enabled

--- database.js
+++ database.js
@@ -11,34 +11,6 @@ async function load(){
         correct = data[0]['correct']
         wrong = data[0]['incorrect']
         prevWrong = wrong
-
-        xhttp = new XMLHttpRequest()
-        xhttp.open("GET","http://worldtimeapi.org/api/timezone/Europe/London",false)
-        await xhttp.send()
-        await xhttp.OPENED
-        currentDate = JSON.parse(xhttp.response)['datetime'].slice(0,10)
-
-        var { data, error } = await supabaseClient
-            .from('fln_time')
-            .select()
-        
-        if(data[0]['time'] != currentDate){
-            var { data, error } = await supabaseClient
-                .from('users')
-                .select()
-                .eq('username','Faulkner')
-            
-            var { data, error } = await supabaseClient
-                .from('users')
-                .update({correct:parseInt(data[0]['correct']) + 500, incorrect:parseInt(data[0]['incorrect'])})
-                .eq('username','Faulkner')
-            
-            var { data, error } = await supabaseClient
-                .from('fln_time')
-                .update({time:currentDate})
-                .eq('id',1)
-        }
-        
     } else {
         location.href = "./login"
     }
