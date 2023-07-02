1a37961ac76dff50b61410b606007c8d57ef3b85
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-07-02 11:40:41 +0100

    Fixed bug with FLN.js not running

--- fln.js
+++ fln.js
@@ -1,26 +1,32 @@
-xhttp = new XMLHttpRequest()
-xhttp.open("GET","http://worldtimeapi.org/api/timezone/Europe/London",false)
-await xhttp.send()
-await xhttp.OPENED
-currentDate = JSON.parse(xhttp.response)['datetime'].slice(0,10)
+async function fln(){
+    xhttp = new XMLHttpRequest()
+    xhttp.open("GET","http://worldtimeapi.org/api/timezone/Europe/London",false)
+    await xhttp.send()
+    await xhttp.OPENED
+    currentDate = JSON.parse(xhttp.response)['datetime'].slice(0,10)
 
-var { data, error } = await supabaseClient
-    .from('fln_time')
-    .select()
-
-if(data[0]['time'] != currentDate){
-    var { data, error } = await supabaseClient
-        .from('users')
-        .select()
-        .eq('username','Faulkner')
-    
-    var { data, error } = await supabaseClient
-        .from('users')
-        .update({correct:parseInt(data[0]['correct']) + 500, incorrect:parseInt(data[0]['incorrect'])})
-        .eq('username','Faulkner')
-    
     var { data, error } = await supabaseClient
         .from('fln_time')
-        .update({time:currentDate})
-        .eq('id',1)
+        .select()
+
+    if(data[0]['time'] != currentDate){
+        var { data, error } = await supabaseClient
+            .from('users')
+            .select()
+            .eq('username','Faulkner')
+        
+        var { data, error } = await supabaseClient
+            .from('users')
+            .update({correct:parseInt(data[0]['correct']) + 500, incorrect:parseInt(data[0]['incorrect'])})
+            .eq('username','Faulkner')
+        
+        var { data, error } = await supabaseClient
+            .from('fln_time')
+            .update({time:currentDate})
+            .eq('id',1)
+    }
+}
+
+window.onload = function(){
+    fln()
 }
