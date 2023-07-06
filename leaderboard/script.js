0fcbb0dd59a8621b5e3ca23390a2a37c0ebbbc76
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2023-07-06 13:23:07 +0000

    Removed incorrect leaderboard

--- leaderboard/script.js
+++ leaderboard/script.js
@@ -27,17 +27,6 @@ async function leaderboard(){
         listElement.innerHTML = `<td>${i + 1}</td><td>${data[i]['username']}</td><td>${data[i]['correct']}</td><td>${data[i]['incorrect']}</td>`
         document.getElementById('leaderboard').appendChild(listElement)
     }
-
-
-    data = Object.values(data).map(value=>value).sort((a, b)=>b['incorrect']-a['incorrect'])
-
-    document.getElementById('leaderboardIncorrect').innerHTML = "<tr><th></th><th>Username</th><th>Correct</th><th>Incorrect</th></tr>"
-
-    for(let i=0;i<data.length;i++){
-        listElement = document.createElement("tr")
-        listElement.innerHTML = `<td>${i + 1}</td><td>${data[i]['username']}</td><td>${data[i]['correct']}</td><td>${data[i]['incorrect']}</td>`
-        document.getElementById('leaderboardIncorrect').appendChild(listElement)
-    }
 }
 
 load()
