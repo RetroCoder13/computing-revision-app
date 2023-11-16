4e0c805b23f29c5c1e9d680ade5de1ca0d6ff277
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-11-16 19:17:44 +0000

    Added questions

--- questions.js
+++ questions.js
@@ -332,6 +332,29 @@ questions = {
             "answer2":"Closed Source software"
         }
     },
+    "MU0":{
+        0:{
+            "question":"What is tempo?",
+            "correctAnswer":"Speed of the music",
+            "answer0":"Volume of the music",
+            "answer1":"Instruments in the music",
+            "answer2":"Notes used in the music"
+        },
+        1:{
+            "question":"What is the key?",
+            "correctAnswer":"Notes used in the music",
+            "answer0":"Instruments in the music",
+            "answer1":"Volume of the music",
+            "answer2":"Speed of the music"
+        },
+        2:{
+            "question":"What are dynamics?",
+            "correctAnswer":"Volume of the music",
+            "answer0":"Notes used in the music",
+            "answer1":"Instruments in the music",
+            "answer2":"Speed of the music"
+        }
+    },
     "MU1":{
         0:{
             "question":"Who composed Brandenburg Concerto No.5?",
