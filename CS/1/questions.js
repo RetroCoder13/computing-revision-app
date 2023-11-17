c245d1df94942640c62d47723c7af76c5e7c706f
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-11-17 16:40:29 +0000

    Test

---
+++ CS/1/questions.js
@@ -0,0 +1,86 @@
+questions = [
+    {
+        "question":"What does MAR stand for?",
+        "correctAnswer":"Memory Address Register",
+        "answer0":"Memory Anchor Register",
+        "answer1":"Memory Architecture Register",
+        "answer2":"Memory Active Register"
+    },
+    {
+        "question":"What does MDR stand for?",
+        "correctAnswer":"Memory Data Register",
+        "answer0":"Memory Date Register",
+        "answer1":"Memory Drone Register",
+        "answer2":"Memory Drain Register"
+    },
+    {
+        "question":"What is the function of the CPU?",
+        "correctAnswer":"Process data and instructions",
+        "answer0":"Store data",
+        "answer1":"Move data from one place to another",
+        "answer2":"Store instructions"
+    },
+    {
+        "question":"Which storage device is best suited for a phone?",
+        "correctAnswer":"Solid State",
+        "answer0":"Optical",
+        "answer1":"Magnetic",
+        "answer2":"Other"
+    },
+    {
+        "question":"Which storage device is a DVD?",
+        "correctAnswer":"Optical",
+        "answer0":"Other",
+        "answer1":"Magnetic",
+        "answer2":"Solid State"
+    },
+    {
+        "question":"What is secondary storage?",
+        "correctAnswer":"Where data that is stored permenantly",
+        "answer0":"Where the boot instructions are stored",
+        "answer1":"Where data the computer is currently using is stored",
+        "answer2":"Where some data the CPU is using frequently is stored"
+    },
+    {
+        "question":"What is primary storage?",
+        "correctAnswer":"Where data the computer is currently using is stored",
+        "answer0":"Where the boot instructions are stored",
+        "answer1":"Where some data the CPU is using frequently is stored",
+        "answer2":"Where data that is stored permenantly"
+    },
+    {
+        "question":"What is an example of secondary storage?",
+        "correctAnswer":"HDD",
+        "answer0":"RAM",
+        "answer1":"CPU",
+        "answer2":"ROM"
+    },
+    {
+        "question":"What is an example of primary storage?",
+        "correctAnswer":"RAM",
+        "answer0":"ROM",
+        "answer1":"HDD",
+        "answer2":"CPU"
+    },
+    {
+        "question":"What is virtual memory?",
+        "correctAnswer":"Memory temporarily stored in the hard drive",
+        "answer0":"Memory the computer is using",
+        "answer1":"Data stored in the hard drive",
+        "answer2":"Memory the CPU is using frequently"
+    },
+    {
+        "question":"What is cache memory?",
+        "correctAnswer":"Memory the CPU is using frequently",
+        "answer0":"Memory temporarily stored in the hard drive",
+        "answer1":"Data stored in the hard drive",
+        "answer2":"Memory the computer is using"
+    },
+    {
+        "question":"What stores the BIOS?",
+        "correctAnswer":"ROM",
+        "answer0":"CPU",
+        "answer1":"RAM",
+        "answer2":"HDD"
+    }
+]
