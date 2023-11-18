ea7cf1110fc7ca92ecf7a44f5046891dfe643726
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-11-18 15:06:59 +0000

    Added some questions for biology topic 1

---
+++ BI/1/questions.js
@@ -0,0 +1,23 @@
+questions = [
+    {
+        "question":"What is the function of a nucleus?",
+        "correctAnswer":"Controlling the cell",
+        "answer0":"Synthesizing proteins",
+        "answer1":"Storing water",
+        "answer2":"Providing energy"
+    },
+    {
+        "question":"What is the function of a mitochondria?",
+        "correctAnswer":"Providing energy",
+        "answer0":"Storing water",
+        "answer1":"Synthesizing proteins",
+        "answer2":"Controlling the cell"
+    },
+    {
+        "question":"What is the function of a ribosome?",
+        "correctAnswer":"Synthesizing proteins",
+        "answer0":"Providing energy",
+        "answer1":"Storing water",
+        "answer2":"Controlling the cell"
+    }
+]
