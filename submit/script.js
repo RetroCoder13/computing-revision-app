f299c6177ee85c62722d8ed610d1f152943d529f
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-11-18 16:37:12 +0000

    Added submission system for questions

---
+++ submit/script.js
@@ -0,0 +1,68 @@
+var username = ""
+
+async function submit(){
+    let questionData = username + "\n" + document.getElementById('subject').value + "\n" + document.getElementById('topic').value + "\n\n"
+    for(let i=0;i<=questions;i++){
+        questionData += ",\n{\n"
+        questionData += '"question": ' + '"' + document.getElementById("question" + i).value + '",\n'
+        questionData += '"correctAnswer": ' + '"' + document.getElementById("correct" + i).value + '",\n'
+        questionData += '"answer0": ' + '"' + document.getElementById("answer0" + i).value + '",\n'
+        questionData += '"answer1": ' + '"' + document.getElementById("answer1" + i).value + '",\n'
+        questionData += '"answer2": ' + '"' + document.getElementById("answer2" + i).value + '",\n'
+        questionData += "}"
+    }
+
+    const { data, error } = await supabaseClient
+        .storage
+        .from('questions')
+        .upload(`questions${new Date()}.txt`,questionData)
+    
+    document.getElementById('message').innerHTML = "Your question has been submitted for approval"
+}
+
+var questions = 0
+function addQuestion(element){
+    questions++
+    let question = document.createElement("input")
+    let correct = document.createElement("input")
+    let answer0 = document.createElement("input")
+    let answer1 = document.createElement("input")
+    let answer2 = document.createElement("input")
+
+    question.id = "question" + questions
+    correct.id = "correct" + questions
+    answer0.id = "answer0" + questions
+    answer1.id = "answer1" + questions
+    answer2.id = "answer2" + questions
+
+    question.placeholder = "Question"
+    correct.placeholder = "Correct Answer"
+    answer0.placeholder = "Other Answer"
+    answer1.placeholder = "Other Answer"
+    answer2.placeholder = "Other Answer"
+
+    element.before(document.createElement("br"))
+    element.before(question)
+    element.before(document.createElement("br"))
+    element.before(correct)
+    element.before(document.createElement("br"))
+    element.before(answer0)
+    element.before(document.createElement("br"))
+    element.before(answer1)
+    element.before(document.createElement("br"))
+    element.before(answer2)
+    element.before(document.createElement("br"))
+    element.before(document.createElement("br"))
+}
+
+window.onload=async function(){
+    if(localStorage.getItem("sb-hxfnilmbkorrzhhnohzp-auth-token")){
+        var { data, error } = await supabaseClient
+            .from('users')
+            .select()
+            .eq('id',JSON.parse(localStorage.getItem("sb-hxfnilmbkorrzhhnohzp-auth-token"))["user"]["id"])
+        username = data[0]['username']
+    } else {
+        location.href = "../login"
+    }
+}
