f0502fdfff011c25895780fcd8f0fdff7a205b9b
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-11-18 17:05:56 +0000

    Added questions for BI5

---
+++ BI/5/questions.js
@@ -0,0 +1,30 @@
+questions = [
+    {
+        "question": "What is the definition of homeostasis?",
+        "correctAnswer": "Maintaining a constant internal environment",
+        "answer0": "Taking in oxygen and removing of carbon dioxide",
+        "answer1": "Keeping a constant temperature",
+        "answer2": "Removal of waste",
+    },
+    {
+        "question": "Which of the following statements is false?",
+        "correctAnswer": "Urea is formed in the kidneys",
+        "answer0": "Water leaves through the lungs when we breathe out",
+        "answer1": "Ions are lost through the skin when we sweat",
+        "answer2": "Carbon Dioxide is a waste product of respiration",
+    },
+    {
+        "question": "Why is it important to maintain a constant body temperature?",
+        "correctAnswer": "Enzymes cannot denature",
+        "answer0": "Prevents bacteria multiplying",
+        "answer1": "Helps bacteria multiply",
+        "answer2": "Makes sure we have enough energy",
+    },
+    {
+        "question": "Which of the following is not a function of the kidneys?",
+        "correctAnswer": "Reabsorbing urea",
+        "answer0": "Filtering the blood",
+        "answer1": "Reabsorbing water",
+        "answer2": "Reabsorbing sugar",
+    }
+]
