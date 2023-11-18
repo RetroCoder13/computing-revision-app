ca3e5104632616dade82949e1c18054e17e8ef98
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-11-18 19:02:58 +0000

    Added questions for MU3

--- MU/3/questions.js
+++ MU/3/questions.js
@@ -5,5 +5,75 @@ questions = [
         "answer0":"Johann Sebastian bach",
         "answer1":"Antonio Vivaldi",
         "answer2":"George Frederic Handel"
+    },
+    {
+        "question":"What era is Music for a While from?",
+        "correctAnswer":"Baroque",
+        "answer0":"Classical",
+        "answer1":"Medieval",
+        "answer2":"Romantic"
+    },
+    {
+        "question":"What key is Music for a While in?",
+        "correctAnswer":"A Minor",
+        "answer0":"A Major",
+        "answer1":"G Minor",
+        "answer2":"C Minor"
+    },
+    {
+        "question":"Which instrument is not featured in Music for a While?",
+        "correctAnswer":"Cello",
+        "answer0":"Harpsichord",
+        "answer1":"Bass Viol",
+        "answer2":"Vocals"
+    },
+    {
+        "question":"What does the left hand of the Harpsichord play?",
+        "correctAnswer":"Ground Bass",
+        "answer0":"Riff",
+        "answer1":"Ostinato",
+        "answer2":"Chords"
+    },
+    {
+        "question":"What word describes when the music reflects the meaning of a word?",
+        "correctAnswer":"Word Painting",
+        "answer0":"Syllabic",
+        "answer1":"Melismatic",
+        "answer2":"Falsetto"
+    },
+    {
+        "question":"What type of voice sings Music for a While?",
+        "correctAnswer":"Soprano",
+        "answer0":"Tenor",
+        "answer1":"Alto",
+        "answer2":"Bass"
+    },
+    {
+        "question":"What is the texture?",
+        "correctAnswer":"Homophonic",
+        "answer0":"Polyphonic",
+        "answer1":"Heterophonic",
+        "answer2":"Monophonic"
+    },
+    {
+        "question":"What technique is used for the dissonance of 'pains'",
+        "correctAnswer":"Word painting",
+        "answer0":"Improvisation",
+        "answer1":"Ornamentation",
+        "answer2":"Melisma"
+    },
+    {
+        "question":"What word describes decorations added to the melody?",
+        "correctAnswer":"Ornamentation",
+        "answer0":"Dissonance",
+        "answer1":"Improvisation",
+        "answer2":"Suspensions"
+    },
+    {
+        "question":"How can the melody be described?",
+        "correctAnswer":"Stepwise",
+        "answer0":"Ascending",
+        "answer1":"Descending",
+        "answer2":"Disjunct"
     }
 ]
