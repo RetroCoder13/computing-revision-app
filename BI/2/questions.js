2655e8bb0e558fd27d44b6eebe6acf4a6df21d1d
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-11-18 18:43:24 +0000

    Added questions for BI2

---
+++ BI/2/questions.js
@@ -0,0 +1,100 @@
+questions = [
+    {
+        "question":"What is the term for a biological catalyst?",
+        "correctAnswer":"Enzyme",
+        "answer0":"Hormone",
+        "answer1":"Protein",
+        "answer2":"Amylase"
+    },
+    {
+        "question":"Name the enzyme that digests starch",
+        "correctAnswer":"Amylase",
+        "answer0":"Protease",
+        "answer1":"Lipase",
+        "answer2":"Pepsin"
+    },
+    {
+        "question":"Name the enzyme that digests fat",
+        "correctAnswer":"Lipase",
+        "answer0":"Protease",
+        "answer1":"Fatase",
+        "answer2":"Amylase"
+    },
+    {
+        "question":"What are the conditions in the stomach like?",
+        "correctAnswer":"Acidic",
+        "answer0":"Alkaline",
+        "answer1":"Neutral",
+        "answer2":"Stormy"
+    },
+    {
+        "question":"Which is the correct size order?",
+        "correctAnswer":"Cell, Tissue, Organ, Organ System, Organism",
+        "answer0":"Tissue, Organ, Cell, Organ System, Organism",
+        "answer1":"Cell, Tissue, Organ System, Organ, Organism",
+        "answer2":"Tissue, Cell, Organism, Organ System, Organ"
+    },
+    {
+        "question":"Where is amylase release?",
+        "correctAnswer":"Salivary Glands and Pancreas",
+        "answer0":"Stomach and Pancreas",
+        "answer1":"Pancreas",
+        "answer2":"Small Intestine and Pancreas"
+    },
+    {
+        "question":"What molecule are enzymes made of?",
+        "correctAnswer":"Protein",
+        "answer0":"Lipids",
+        "answer1":"Fatty Acids",
+        "answer2":"Glucose"
+    },
+    {
+        "question":"What is the active site of an enzyme?",
+        "correctAnswer":"Where the substrate binds",
+        "answer0":"Where the product binds",
+        "answer1":"Where the reaction is completed",
+        "answer2":"Where the reaction is catalysed"
+    },
+    {
+        "question":"Where is bile made?",
+        "correctAnswer":"Liver",
+        "answer0":"Gall Bladder",
+        "answer1":"Small Intestine",
+        "answer2":"Pancreas"
+    },
+    {
+        "question":"Where does the left atrium pump blood to?",
+        "correctAnswer":"Left ventricle",
+        "answer0":"Right atrium",
+        "answer1":"Right ventricle",
+        "answer2":"Lungs"
+    },
+    {
+        "question":"What is the role of coronary arteries?",
+        "correctAnswer":"Supply oxygenated blood to the heart muscle",
+        "answer0":"Supply deoxygenated blood to the heart muscle",
+        "answer1":"Supply oxygenated blood to the lungs",
+        "answer2":"Supply deoxygenated blood to the muscles"
+    },
+    {
+        "question":"What is the role of platelets?",
+        "correctAnswer":"Form clots to reduce bleeding",
+        "answer0":"Carry hormones and waste",
+        "answer1":"Fight off infections",
+        "answer2":"Bind to oxygen"
+    },
+    {
+        "question":"What is transported through active transport?",
+        "correctAnswer":"Glucose",
+        "answer0":"Oxygen",
+        "answer1":"Starch",
+        "answer2":"Carbon Dioxide"
+    },
+    {
+        "question":"What is transpiration?",
+        "correctAnswer":"Movement of water and mineral ions from the roots",
+        "answer0":"Movement of glucose from the roots",
+        "answer1":"Movement of starch to the roots",
+        "answer2":"Movement of water and mineral ions to the roots"
+    }
+]
