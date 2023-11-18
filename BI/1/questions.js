93133a1c8b04f42fdc85b85acaa07319dd7e4c3b
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-11-18 15:22:47 +0000

    Added more questions

--- BI/1/questions.js
+++ BI/1/questions.js
@@ -19,5 +19,40 @@ questions = [
         "answer0":"Providing energy",
         "answer1":"Storing water",
         "answer2":"Controlling the cell"
+    },
+    {
+        "question":"What is the function of the cell membrane?",
+        "correctAnswer":"Controlling what can enter and exit the cell",
+        "answer0":"Photosynthesis",
+        "answer1":"Storing water",
+        "answer2":"Controlling the cell"
+    },
+    {
+        "question":"Where does photosynthesis occur in plants?",
+        "correctAnswer":"Chloroplast",
+        "answer0":"Nucleus",
+        "answer1":"Cytoplasm",
+        "answer2":"Cell Membrane"
+    },
+    {
+        "question":"What provides energy in a cell?",
+        "correctAnswer":"Mitochondria",
+        "answer0":"Ribosome",
+        "answer1":"Nucleus",
+        "answer2":"Cytoplasm"
+    },
+    {
+        "question":"What controls the cell?",
+        "correctAnswer":"Nucleus",
+        "answer0":"Mitochondria",
+        "answer1":"Cytoplasm",
+        "answer2":"Ribosome"
+    },
+    {
+        "question":"What synthesizes proteins?",
+        "correctAnswer":"Ribosome",
+        "answer0":"Nucleus",
+        "answer1":"Mitochondria",
+        "answer2":"Cytoplasm"
     }
 ]
