a184702cf9a95a0bc1272937b28ffaf29bca18cb
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-11-19 14:08:12 +0000

    Added questions for BI4

---
+++ BI/4/questions.js
@@ -0,0 +1,58 @@
+questions = [
+    {
+        "question": "When do plants photosynthesise?",
+        "correctAnswer": "During the day",
+        "answer0": "All the time",
+        "answer1": "During the night",
+        "answer2": "Plants don't photosynthesise",
+    },
+    {
+        "question": "What do plants need to photosynthesise?",
+        "correctAnswer": "Light",
+        "answer0": "Glucose",
+        "answer1": "Oxygen",
+        "answer2": "Nutrients",
+    },
+    {
+        "question": "What two products are made from phtosynthesis?",
+        "correctAnswer": "Oxygen and Glucose",
+        "answer0": "Carbon Dioxide and Water",
+        "answer1": "Oxygen and Water",
+        "answer2": "Glucose and Carbon Dioxide",
+    },
+    {
+        "question": "What do plants do with excess glucose?",
+        "correctAnswer": "Store it as starch",
+        "answer0": "Make new cells",
+        "answer1": "Use it in respiration",
+        "answer2": "Remove it through roots",
+    },
+    {
+        "question": "What is the process we use to get energy?",
+        "correctAnswer": "Respiration",
+        "answer0": "Photosynthesis",
+        "answer1": "Digestion",
+        "answer2": "Excretion",
+    },
+    {
+        "question": "After exercise you breathe heavily because of a what?",
+        "correctAnswer": "Oxygen Debt",
+        "answer0": "Oxygen Shortage",
+        "answer1": "Oxygen Loan",
+        "answer2": "Oxygen Withdrawal",
+    },
+    {
+        "question": "What transports oxygen around the body?",
+        "correctAnswer": "Red Blood Cells",
+        "answer0": "Plasma",
+        "answer1": "Platelets",
+        "answer2": "White Blood Cells",
+    },
+    {
+        "question": "What does not affect metabolic rate?",
+        "correctAnswer": "Mood",
+        "answer0": "Age",
+        "answer1": "Gender",
+        "answer2": "Exercise",
+    }
+]
