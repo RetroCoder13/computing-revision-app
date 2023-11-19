73d3234baec5a017e390e892a4b88a56c2f5031c
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-11-19 11:52:05 +0000

    Added questions for BI6

---
+++ BI/6/questions.js
@@ -0,0 +1,9 @@
+questions = [
+    {
+        "question": "What is mRNA",
+        "correctAnswer": "A single-stranded molecule of RNA",
+        "answer0": "A molecule that serves as a link between a single-stranded molecule of RNA and amino acids",
+        "answer1": "A sequence of proteins",
+        "answer2": "A polymer made of many amino acids joined together",
+    }
+]
