60927264e8dd994c897c8a84d3db91bf4bc80144
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2023-11-20 08:54:09 +0000

    Updated answer

--- BI/6/questions.js
+++ BI/6/questions.js
@@ -1,7 +1,7 @@
 questions = [
     {
         "question": "What is mRNA",
-        "correctAnswer": "A single-stranded molecule of RNA",
+        "correctAnswer": "A single-stranded molecule of DNA",
         "answer0": "A molecule that serves as a link between a single-stranded molecule of RNA and amino acids",
         "answer1": "A sequence of proteins",
         "answer2": "A polymer made of many amino acids joined together",
