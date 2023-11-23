4e2d1244896f6d1d70b5ee3b9ee47313e5e2b874
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2023-11-23 11:50:41 +0000

    Added questions for CS6

---
+++ CS/6/questions.js
@@ -0,0 +1,72 @@
+questions = [
+    {
+        "question":"Which one is a searching algorithm?",
+        "correctAnswer":"Linear Search",
+        "answer0":"Insertion Search",
+        "answer1":"Bubble Search",
+        "answer2":"Binary Search"
+    },
+    {
+        "question":"Which is not a sorting algorithm?",
+        "correctAnswer":"Binary Sort",
+        "answer0":"Bubble Sort",
+        "answer1":"Merge Sort",
+        "answer2":"Insertion Sort"
+    },
+    {
+        "question":"Which sorting algorithm is the fastest?",
+        "correctAnswer":"Merge Sort",
+        "answer0":"Binary Sort",
+        "answer1":"Insertion Sort",
+        "answer2":"Bubble Sort"
+    },
+    {
+        "question":"What is the most number of passes needed to search a list of 12 items using a bubble sort",
+        "correctAnswer":"11",
+        "answer0":"10",
+        "answer1":"12",
+        "answer2":"13"
+    },
+    {
+        "question":"What is the most number of passes needed to search a list of n items using a bubble sort",
+        "correctAnswer":"n-1",
+        "answer0":"2n",
+        "answer1":"n+1",
+        "answer2":"n"
+    },
+    {
+        "question":"How many passes does an insertion sort take of n items?",
+        "correctAnswer":"1",
+        "answer0":"2n",
+        "answer1":"n",
+        "answer2":"n+1"
+    },
+    {
+        "question":"What is the fastest searching algorithm?",
+        "correctAnswer":"Binary Search",
+        "answer0":"Insertion Search",
+        "answer1":"Bubble Search",
+        "answer2":"Linear Search"
+    },
+    {
+        "question":"What is abstraction?",
+        "correctAnswer":"Removing unnecessary detail from a problem",
+        "answer0":"Breaking down a large problem into smaller steps",
+        "answer1":"An algorithm",
+        "answer2":"Set of instructions for completing a task"
+    },
+    {
+        "question":"What is decomposition?",
+        "correctAnswer":"Breaking down a large problem into smaller steps",
+        "answer0":"Set of instructions for completing a task",
+        "answer1":"Removing unnecessary detail from a problem",
+        "answer2":"An algorithm"
+    },
+    {
+        "question":"What is an algorithm?",
+        "correctAnswer":"Set of instructions for completing a task",
+        "answer0":"Breaking down a large problem into smaller steps",
+        "answer1":"Removing unnecessary detail from a problem",
+        "answer2":"Array"
+    }
+]
