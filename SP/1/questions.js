cbf989caf76f30413b0fdc98238bf7e77ad0209f
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-12-01 16:59:37 +0000

    Added Spanish Language questions

---
+++ SP/1/questions.js
@@ -0,0 +1,16 @@
+questions = [
+    {
+        "question": "What does 'Siempre he querido' mean?",
+        "correctAnswer": "I have always wanted to",
+        "answer0": "I have dreamed of",
+        "answer1": "I really want to",
+        "answer2": "I have to",
+    },
+    {
+        "question": "What does 'Suelo jugar' mean?",
+        "correctAnswer": "I usually play",
+        "answer0": "I used to play",
+        "answer1": "I usually judge",
+        "answer2": "I used to judge",
+    }
+]
