a83ba7808cf1b376696702f9026070bb3d3f8a55
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-12-02 11:51:29 +0000

    Added questions for PH3 and PH4

---
+++ PH/3/questions.js
@@ -0,0 +1,16 @@
+questions = [
+    {
+        "question": "What name is given to the total kinetic and potential energy of all the particles of helium gas in a balloon",
+        "correctAnswer": "Internal Energy",
+        "answer0": "Movement Energy",
+        "answer1": "Thermal Energy",
+        "answer2": "External Energy",
+    },
+    {
+        "question": "How is heat transferred through the walls of a freezer?",
+        "correctAnswer": "Conduction",
+        "answer0": "Radiation",
+        "answer1": "Convection",
+        "answer2": "Electromagnetic waves",
+    }
+]
