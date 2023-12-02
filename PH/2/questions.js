263e3af65e0b8736d0644e5e4508121250340b9e
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-12-02 11:26:09 +0000

    Added questions for PH2

---
+++ PH/2/questions.js
@@ -0,0 +1,58 @@
+questions = [
+    {
+        "question": "What is the frequency of UK AC supply?",
+        "correctAnswer": "50Hz",
+        "answer0": "60Hz",
+        "answer1": "100Hz",
+        "answer2": "120Hz",
+    },
+    {
+        "question": "What type of current does a battery supply?",
+        "correctAnswer": "Direct Current",
+        "answer0": "Both AC and DC",
+        "answer1": "Alternating Current",
+        "answer2": "Niether AC nor DC",
+    },
+    {
+        "question": "What is the colour of the live wire in UK plugs?",
+        "correctAnswer": "Brown",
+        "answer0": "Red",
+        "answer1": "Yellow and Green",
+        "answer2": "Blue",
+    },
+    {
+        "question": "What is the colour of the neutral wire in UK plugs?",
+        "correctAnswer": "Blue",
+        "answer0": "Yellow and Green",
+        "answer1": "Brown",
+        "answer2": "Red",
+    },
+    {
+        "question": "What is the colour of the earth wire in UK plugs?",
+        "correctAnswer": "Yellow and Green",
+        "answer0": "Blue",
+        "answer1": "Red",
+        "answer2": "Brown",
+    },
+    {
+        "question": "What is charge measured in?",
+        "correctAnswer": "Coulombs",
+        "answer0": "Amps",
+        "answer1": "Joules",
+        "answer2": "Watts",
+    },
+    {
+        "question": "What is the national grid?",
+        "correctAnswer": "A system of cables and transformers",
+        "answer0": "A system of transformers",
+        "answer1": "A system of cables and pylons",
+        "answer2": "A system of transformers, cables and power stations",
+    },
+    {
+        "question": "What happens to the resistance of a lamp as voltage increases?",
+        "correctAnswer": "Increases",
+        "answer0": "Stays the same",
+        "answer1": "Halves",
+        "answer2": "Decreases",
+    }
+]
