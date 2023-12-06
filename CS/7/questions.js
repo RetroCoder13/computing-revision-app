79dcfa118fdba34d065705fbadabd20282bec2c3
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2023-12-06 12:12:02 +0000

    Added some questions for CS7

---
+++ CS/7/questions.js
@@ -0,0 +1,58 @@
+questions = [
+    {
+        "question": "What is a variable?",
+        "correctAnswer": "Named memory address that holds a value",
+        "answer0": "A function in Python",
+        "answer1": "A programming construct",
+        "answer2": "A type of selection statement",
+    },
+    {
+        "question": "How could you assign a variable?",
+        "correctAnswer": "<code>variable = value</code>",
+        "answer0": "<code>value = variable</code>",
+        "answer1": "<code>'variable' = 'value'</code>",
+        "answer2": "<code>'value' = 'variable'</code>",
+    },
+    {
+        "question": "What is a constant?",
+        "correctAnswer": "A variable that cannot be changed",
+        "answer0": "A number",
+        "answer1": "Something that is in upper case",
+        "answer2": "A variable that can be changed",
+    },
+    {
+        "question": "What function is used to display a value in Python?",
+        "correctAnswer": "<code>print()</code>",
+        "answer0": "<code>input()</code>",
+        "answer1": "<code>display()</code>",
+        "answer2": "<code>output()</code>",
+    },
+    {
+        "question": "What function is used for the user to input a value?",
+        "correctAnswer": "<code>input()</code>",
+        "answer0": "<code>display()</code>",
+        "answer1": "<code>output()</code>",
+        "answer2": "<code>print()</code>",
+    },
+    {
+        "question": "What is the arithmetic operator for MOD in Python?",
+        "correctAnswer": "<code>%</code>",
+        "answer0": "<code>/</code>",
+        "answer1": "<code>MOD</code>",
+        "answer2": "<code>$</code>",
+    },
+    {
+        "question": "What does the boolean operator <code>==</code> mean in Python?",
+        "correctAnswer": "Equal to",
+        "answer0": "Larger than",
+        "answer1": "True",
+        "answer2": "Less than",
+    },
+    {
+        "question": "What is the error in the following IF statement in Python:<br><code>if variable = True:</code>",
+        "correctAnswer": "Syntax error: Should be <code>==</code>",
+        "answer0": "Logical error: <code>variable</code> can never be True",
+        "answer1": "Logical error: True is a bad variable name",
+        "answer2": "Syntax error: True is not a valid value",
+    }
+]
