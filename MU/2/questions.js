1641a39d4db632ff12ca8a448b351c91045f172c
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2023-12-07 12:26:15 +0000

    Added more questions for MU2

--- MU/2/questions.js
+++ MU/2/questions.js
@@ -5,5 +5,47 @@ questions = [
         "answer0":"Henry Purcell",
         "answer1":"George Frederic Handel",
         "answer2":"Johann Sebastian Bach"
+    },
+    {
+        "question": "What key is this piece in?",
+        "correctAnswer": "C minor",
+        "answer0": "E minor",
+        "answer1": "D major",
+        "answer2": "C major",
+    },
+    {
+        "question": "How does the sonata begin?",
+        "correctAnswer": "Slow introduction",
+        "answer0": "Exposition",
+        "answer1": "Forte",
+        "answer2": "Fast introduction",
+    },
+    {
+        "question": "What is the first theme in the exposition called?",
+        "correctAnswer": "First Subject",
+        "answer0": "Introduction",
+        "answer1": "Second Subject",
+        "answer2": "Transition",
+    },
+    {
+        "question": "What section is heard last in a sonata?",
+        "correctAnswer": "Recapitulation",
+        "answer0": "Exposition",
+        "answer1": "Development",
+        "answer2": "Subjects",
+    },
+    {
+        "question": "What is the tempo of the opening?",
+        "correctAnswer": "Grave",
+        "answer0": "Allegro",
+        "answer1": "Andante",
+        "answer2": "Presto",
+    },
+    {
+        "question": "What is the recapitulation?",
+        "correctAnswer": "The final section",
+        "answer0": "The second section",
+        "answer1": "The first section",
+        "answer2": "The subjects",
     }
 ]
