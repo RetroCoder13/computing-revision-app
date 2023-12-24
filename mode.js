f727cea5c1136df58c833808d0c685cf14e9acf3
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-12-24 11:34:14 +0000

    Dark mode available on all pages

--- mode.js
+++ mode.js
@@ -9,11 +9,11 @@ function loadMode(){
     if(darkMode){
         document.documentElement.style.setProperty("--mode-bg","#111111")
         document.documentElement.style.setProperty("--mode-text","white")
-        document.getElementById("darkMode").src = "sun.png"
+        document.getElementById("darkMode").src = "https://retrocoder13.github.io/revision/sun.png"
     } else {
         document.documentElement.style.setProperty("--mode-bg","#FFFFFF")
         document.documentElement.style.setProperty("--mode-text","black")
-        document.getElementById("darkMode").src = "moon.png"
+        document.getElementById("darkMode").src = "https://retrocoder13.github.io/revision/moon.png"
     }
 }
 
@@ -22,12 +22,12 @@ function changeMode(){
         darkMode = false
         document.documentElement.style.setProperty("--mode-bg","#FFFFFF")
         document.documentElement.style.setProperty("--mode-text","black")
-        document.getElementById("darkMode").src = "moon.png"
+        document.getElementById("darkMode").src = "https://retrocoder13.github.io/revision/moon.png"
     } else {
         darkMode = true
         document.documentElement.style.setProperty("--mode-bg","#111111")
         document.documentElement.style.setProperty("--mode-text","white")
-        document.getElementById("darkMode").src = "sun.png"
+        document.getElementById("darkMode").src = "https://retrocoder13.github.io/revision/sun.png"
     }
     localStorage["revision-darkMode"] = darkMode
 }
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.onload=function(){
     load()
-    loadMode()
     // var request = new XMLHttpRequest()
     // request.open("GET","https://raw.githubusercontent.com/RetroCoder13/revision/questions/questions.js",false);
     // request.send()
