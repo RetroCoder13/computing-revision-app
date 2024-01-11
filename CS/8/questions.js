75da9e987437572c337f6f3801894c14a9c367ce
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-01-11 12:17:29 +0000

    Added questions for CS8

---
+++ CS/8/questions.js
@@ -0,0 +1,23 @@
+questions = [
+    {
+        "question": "What is the error in the code? <br><code>age = input('What is your age? '</code>",
+        "correctAnswer": "Syntax Error - Missing bracket",
+        "answer0": "Logic Error - Input asks for name and saves it to <code>age</code>",
+        "answer1": "Syntax Error - Expected Indent",
+        "answer2": "Syntax Error - <code>age</code> is not defined",
+    },
+    {
+        "question": "What is the error in the code? <br><code>age = input('What is your name? ')</code>",
+        "correctAnswer": "Logic Error - Input asks for name and saves it to <code>age</code>",
+        "answer0": "Syntax Error - <code>age</code> is not defined",
+        "answer1": "Syntax Error - Missing bracket",
+        "answer2": "Syntax Error - Expected Indent",
+    },
+    {
+        "question": "What is the error in the code? <br><code>area = length + height</code>",
+        "correctAnswer": "Logic Error - A<code>area</code> as the sum of <code>length</code> and <code>height</code>",
+        "answer0": "Syntax Error - Expected indent",
+        "answer1": "Logic Error - A<code>area</code> as the product of <code>length</code> and <code>height</code>",
+        "answer2": "Syntax Error - <code>area</code> is not defined",
+    }
+]
