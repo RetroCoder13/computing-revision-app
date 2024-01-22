b8625841fa101f21b14c2f176440aed98a295af2
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-01-22 14:01:50 +0000

    Added questions for BI7

---
+++ BI/7/questions.js
@@ -0,0 +1,72 @@
+questions = [
+    {
+        "question": "What are the types of adaptation?",
+        "correctAnswer": "Behavioural, Functional and Structural",
+        "answer0": "Behavioural and Structural",
+        "answer1": "Behavioural, Physical, Functional",
+        "answer2": "Functional",
+    },
+    {
+        "question": "What is interdependence?",
+        "correctAnswer": "Different organisms rely on each other for survival",
+        "answer0": "Members of a species competing for resources",
+        "answer1": "Organisms fighting each other",
+        "answer2": "Members of a species rely on each other"
+    },
+    {
+        "question": "Define the term ecosystem",
+        "correctAnswer": "Environmental interactions between all organisms",
+        "answer0": "All organisms in a habitiat",
+        "answer1": "Place where organisms live",
+        "answer2": "Plants living in a system",
+    },
+    {
+        "question": "What do organisms compete for?",
+        "correctAnswer": "Food, Water, Shelter, Mates, Territory, Light",
+        "answer0": "Food, Water and Mates",
+        "answer1": "Mates and Territory",
+        "answer2": "Food, Shelter, Mates, Territory, Light",
+    },
+    {
+        "question": "What is an example of an abiotic factor?",
+        "correctAnswer": "Light Intensity",
+        "answer0": "Predators",
+        "answer1": "Disease",
+        "answer2": "Human Presence",
+    },
+    {
+        "question": "What is an example of a biotic factor?",
+        "correctAnswer": "Disease",
+        "answer0": "Rainfall",
+        "answer1": "Carbo Dioxide Concentration",
+        "answer2": "Light Intensity",
+    },
+    {
+        "question": "What is an extremophile?",
+        "correctAnswer": "An organism adapted to live in extreme conditions",
+        "answer0": "A plant that lives in the desert",
+        "answer1": "An organism that does not live in an extreme condition",
+        "answer2": "A plant that is dead",
+    },
+    {
+        "question": "What is an omnivore?",
+        "correctAnswer": "Organisms that eats plants and animals",
+        "answer0": "Organisms that eats animals",
+        "answer1": "Organisms that eats plants",
+        "answer2": "Plants and animals",
+    },
+    {
+        "question": "What does a pyramid of biomass show?",
+        "correctAnswer": "The transfer of biomass through a food chain",
+        "answer0": "The number of difference species in a food chain",
+        "answer1": "The transfer of species through a food chain",
+        "answer2": "The biomass of the species in a food chain",
+    },
+    {
+        "question": "What is food security?",
+        "correctAnswer": "Having enough food to feed a population",
+        "answer0": "Protecting food from burglars",
+        "answer1": "Having enough food to feed a family",
+        "answer2": "Protecting food from the weather",
+    }
+]
