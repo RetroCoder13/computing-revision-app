0aa2eff514d6e7add317048993651e96482af767
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2024-01-28 18:59:26 +0000

    Inverts loading icon based on mode

--- mode.js
+++ mode.js
@@ -11,11 +11,13 @@ function loadMode(){
         document.documentElement.style.setProperty("--mode-text","white")
         document.getElementById("darkMode").src = "https://retrocoder13.github.io/revision/sun.svg"
         document.getElementById("darkMode").style.filter = "invert(1)"
+        document.getElementById("loading").style.filter = "invert(1)"
     } else {
         document.documentElement.style.setProperty("--mode-bg","#FFFFFF")
         document.documentElement.style.setProperty("--mode-text","black")
         document.getElementById("darkMode").src = "https://retrocoder13.github.io/revision/moon.svg"
         document.getElementById("darkMode").style.filter = "invert(0)"
+        document.getElementById("loading").style.filter = "invert(0)"
     }
 }
 
@@ -26,12 +28,14 @@ function changeMode(){
         document.documentElement.style.setProperty("--mode-text","black")
         document.getElementById("darkMode").src = "https://retrocoder13.github.io/revision/moon.svg"
         document.getElementById("darkMode").style.filter = "invert(0)"
+        document.getElementById("loading").style.filter = "invert(0)"
     } else {
         darkMode = true
         document.documentElement.style.setProperty("--mode-bg","#111111")
         document.documentElement.style.setProperty("--mode-text","white")
         document.getElementById("darkMode").src = "https://retrocoder13.github.io/revision/sun.svg"
         document.getElementById("darkMode").style.filter = "invert(1)"
+        document.getElementById("loading").style.filter = "invert(1)"
     }
     localStorage["revision-darkMode"] = darkMode
 }
