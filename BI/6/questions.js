a68790e8667260f28abdef2084d183f2321361a5
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-01-29 14:43:04 +0000

    Added questions to BI6

--- BI/6/questions.js
+++ BI/6/questions.js
@@ -5,5 +5,19 @@ questions = [
         "answer0": "A molecule that serves as a link between a single-stranded molecule of RNA and amino acids",
         "answer1": "A sequence of proteins",
         "answer2": "A polymer made of many amino acids joined together",
+    },
+    {
+        "question": "Why does a dominant allele create a higher probability of an offspring affected by disease?",
+        "correctAnswer": "Only one dominant allele is needed for a 50% probability",
+        "answer0": "Parents always pass on dominant alleles",
+        "answer1": "Dominant alleles are more common than recessive alleles",
+        "answer2": "Dominant alleles always cause disease",
+    },
+    {
+        "question": "Why does cystic fibrosis need two alleles to cause the disease?",
+        "correctAnswer": "It is a recessive disorder so you need two recessive alleles",
+        "answer0": "You need both a dominant and a recessive allele",
+        "answer1": "It is a dominant disorder so you need two dominant alleles",
+        "answer2": "It is a dominant disorder so it needs at least one dominant allele",
     }
 ]
