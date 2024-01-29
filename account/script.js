8861e95ffa729caea238e457a643cdbad8cd1eca
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-01-29 12:26:44 +0000

    Redirect

--- account/script.js
+++ account/script.js
@@ -2,12 +2,12 @@ async function changeEmail(){
     const { data, error } = await supabaseClient.auth.updateUser({
         email: document.getElementById('email').value
     })
-    document.getElementById('error').innerHTML = error
+    location.href = "../../"
 }
 
 async function changePassword(){
     const { data, error } = await supabaseClient.auth.updateUser({
         password: document.getElementById('password').value
     })
-    document.getElementById('error').innerHTML = error
+    location.href = "../../"
 }
