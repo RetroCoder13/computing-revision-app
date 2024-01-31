f99afbf047ddf1a075e9e8a3d8474af1ba4bf643
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-01-31 12:12:33 +0000

    Added banned emojis

--- emoji.js
+++ emoji.js
@@ -17,9 +17,23 @@ async function loadEmoji(){
 async function changeEmoji(){
     username = document.getElementById('username').innerHTML
     emojiCode = document.getElementById('emojiCode').value
-    var { data, error } = await supabaseClient
-        .from('users')
-        .update({emoji:emojiCode})
-        .eq('id',JSON.parse(localStorage.getItem("sb-hxfnilmbkorrzhhnohzp-auth-token"))["user"]["id"])
-    loadEmoji()
+    if(!bannedEmojis.includes(emojiCode)){
+        var { data, error } = await supabaseClient
+            .from('users')
+            .update({emoji:emojiCode})
+            .eq('id',JSON.parse(localStorage.getItem("sb-hxfnilmbkorrzhhnohzp-auth-token"))["user"]["id"])
+        loadEmoji()
+    } else {
+        document.getElementById('emojiCode').value = ""
+        document.getElementById('emojiCode').placeholder = "Blocked emoji"
+    }
 }
+
+const bannedEmojis = [
+    "1F595", // Middle finger
+    "1F346", // Eggplant
+    "1F351", // Peach
+    "1F352", // Cherries
+    "1F64B", // Person raising hand
+    "1F4A6", // Sweat droplets
+]
