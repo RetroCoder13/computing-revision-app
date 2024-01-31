97cfb7a8b8f899b3d7ab8d14848784c413cd51a4
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-01-31 12:03:34 +0000

    Started to fix icons on the leaderboard

--- leaderboard/script.js
+++ leaderboard/script.js
@@ -8,7 +8,7 @@ async function load(){
             .subscribe()
         leaderboard()
     } else {
-        location.href = "../login"
+        location.href = "../auth/signin"
     }
 }
 
@@ -26,7 +26,7 @@ async function leaderboard(){
     for(let i=0;i<data.length;i++){
         listElement = document.createElement("tr")
         if(data[i]['emoji'] != "" && data[i]['emoji'] != null){
-            listElement.innerHTML = `<td>${i + 1}</td><td><img src="${"https://openmoji.org/data/color/svg/" + data[i]['emoji'] + ".svg"}" height="15"> ${data[i]['username']} <img src="${"https://openmoji.org/data/color/svg/" + data[i]['emoji'] + ".svg"}" height="15"></td><td>${data[i]['correct']}</td><td>${data[i]['incorrect']}</td>`
+            listElement.innerHTML = `<td>${i + 1}</td><td><img src="${"https://openmoji.org/data/color/svg/" + data[i]['emoji'] + ".svg"}" height="30" class="leftImg">${data[i]['username']}<img src="${"https://openmoji.org/data/color/svg/" + data[i]['emoji'] + ".svg"}" height="30" class="rightImg"></td><td>${data[i]['correct']}</td><td>${data[i]['incorrect']}</td>`
         } else {
             listElement.innerHTML = `<td>${i + 1}</td><td>${data[i]['username']}</td><td>${data[i]['correct']}</td><td>${data[i]['incorrect']}</td>`
         }
@@ -34,4 +34,6 @@ async function leaderboard(){
     }
 }
 
-load()
+window.onload=function(){
+    load()
+}
