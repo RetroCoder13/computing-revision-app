5540a0d122aa631fc433e3ddf7602fd4524e7d3e
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-02-01 14:45:28 +0000

    Added questions to CH10

---
+++ CH/10/questions.js
@@ -0,0 +1,30 @@
+questions = [
+  {
+    "question": "What are ceramics?",
+    "correctAnswer": "Non-metal solids with high melting points",
+    "answer0": "A material embedded with another",
+    "answer1": "Polymer chains entwined together with weak forces",
+    "answer2": "Polymer chains entwined together with strong forces",
+  },
+  {
+      "question": "What are composites?",
+      "correctAnswer": "A material embedded with another",
+      "answer0": "Polymer chains entwined together with strong forces",
+      "answer1": "Polymer chains entwined together with weak forces",
+      "answer2": "Non-metal solids with high melting points",
+  },
+  {
+      "question": "What are thermosoftening polymers?",
+      "correctAnswer": "Polymer chains entwined together with weak forces",
+      "answer0": "Non-metal solids with high melting points",
+      "answer1": "Polymer chains entwined together with strong forces",
+      "answer2": "A material embedded with another",
+  },
+  {
+      "question": "What are thermosetting polymers?",
+      "correctAnswer": "Polymer chains entwined together with strong forces",
+      "answer0": "Polymer chains entwined together with weak forces",
+      "answer1": "A material embedded with another",
+      "answer2": "Non-metal solids with high melting points",
+  }
+]
