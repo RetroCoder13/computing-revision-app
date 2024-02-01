3080f8d1187a3482cc91543ac297234139da2890
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-02-01 14:57:41 +0000

    Added questions to MU4

--- MU/4/questions.js
+++ MU/4/questions.js
@@ -5,5 +5,19 @@ questions = [
         "answer0":"AC/DC",
         "answer1":"The Beatles",
         "answer2":"Pink Floyd"
+    },
+    {
+        "question": "What key is Killer Queen in during choruses?",
+        "correctAnswer": "Bb major",
+        "answer0": "C minor",
+        "answer1": "Eb major",
+        "answer2": "F# minor",
+    },
+    {
+        "question": "What is the tonic key of Killer Queen?",
+        "correctAnswer": "Eb major",
+        "answer0": "Bb major",
+        "answer1": "C minor",
+        "answer2": "C major",
     }
 ]
