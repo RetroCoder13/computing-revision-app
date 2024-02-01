f84bdb7b773253d9baeb7c05623c4f3ab6240cd2
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-02-01 11:39:48 +0000

    Updated code to change mode

--- mode.js
+++ mode.js
@@ -28,26 +28,7 @@ function loadMode(){
 }
 
 function changeMode(){
-    if(darkMode){
-        darkMode = false
-        document.documentElement.style.setProperty("--mode-bg","#FFFFFF")
-        document.documentElement.style.setProperty("--mode-text","black")
-        document.getElementById("darkMode").src = "https://retrocoder13.github.io/revision/moon.svg"
-        document.getElementById("darkMode").style.filter = "invert(0)"
-        document.getElementById("loading").style.filter = "invert(0)"
-        if(document.getElementById("accountIcon").src.includes("account.svg")){
-            document.getElementById("accountIcon").style.filter = "invert(0)"
-        }
-    } else {
-        darkMode = true
-        document.documentElement.style.setProperty("--mode-bg","#111111")
-        document.documentElement.style.setProperty("--mode-text","white")
-        document.getElementById("darkMode").src = "https://retrocoder13.github.io/revision/sun.svg"
-        document.getElementById("darkMode").style.filter = "invert(1)"
-        document.getElementById("loading").style.filter = "invert(1)"
-        if(document.getElementById("accountIcon").src.includes("account.svg")){
-            document.getElementById("accountIcon").style.filter = "invert(1)"
-        }
-    }
+    darkMode = !darkMode
+    loadMode()
     localStorage["revision-darkMode"] = darkMode
 }
