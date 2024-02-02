646a0172893f6860d312c7206131dd2c596498f2
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-02-02 12:04:15 +0000

    Added question to SP7

---
+++ SP/7/questions.js
@@ -0,0 +1,9 @@
+questions = [
+  {
+    "question": "'el medio ambiente'",
+    "correctAnswer": "The environment",
+    "answer0": "Climate change",
+    "answer1": "Global Warming",
+    "answer2": "Archie and Ella :)",
+  }
+]
