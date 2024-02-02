713b8b8ce23a7ef7c1ca7f85b94f85aa9b72e4ae
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-02-02 12:06:02 +0000

    Added questions to SP7

--- SP/7/questions.js
+++ SP/7/questions.js
@@ -5,5 +5,26 @@ questions = [
     "answer0": "Climate change",
     "answer1": "Global Warming",
     "answer2": "Archie and Ella :)",
+  },
+  {
+      "question": "What is 'global warming' in Spanish",
+      "correctAnswer": "el calentamiento global",
+      "answer0": "el cambio climático",
+      "answer1": "el intercambio global",
+      "answer2": "el atenuación global",
+  },
+  {
+      "question": "What is 'climate change' in Spanish",
+      "correctAnswer": "el cambio climático",
+      "answer0": "el calentamiento global",
+      "answer1": "el intercambio global",
+      "answer2": "el atenuación global",
+  },
+  {
+      "question": "What does 'la sequía' mean",
+      "correctAnswer": "drought",
+      "answer0": "flood",
+      "answer1": "forest",
+      "answer2": "woodlands",
   }
 ]
