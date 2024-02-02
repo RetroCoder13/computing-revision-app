550d237e7c54343388934fb9b3ea2c98693e6b45
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2024-02-02 17:04:08 +0000

    You can now click off the account menu

--- account.js
+++ account.js
@@ -9,3 +9,9 @@ function account(){
         document.getElementById('accountInfo').style.animation = "pop-in .5s forwards"
     }
 }
+
+document.addEventListener('mousedown',function(e){
+    if(accountMenu == true && (e.clientX > 200 || (e.clientX > 50 && e.clientY < 50))){
+        account()
+    }
+})
