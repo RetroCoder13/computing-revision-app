642e9edbc4ffb81017f9f695ae243db711a64dc2
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-02-02 12:02:59 +0000

    Moved question to SP3

---
+++ SP/3/questions.js
@@ -0,0 +1,9 @@
+questions = [
+  {
+        "question": "What does 'Suelo jugar' mean?",
+        "correctAnswer": "I usually play",
+        "answer0": "I used to play",
+        "answer1": "I usually judge",
+        "answer2": "I used to judge",
+    }
+]
