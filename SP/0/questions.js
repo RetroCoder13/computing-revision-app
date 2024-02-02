bcbaaf744e1e46a87c1ef3b543b7c6d9b332eb1b
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-02-02 12:02:09 +0000

    Moved question to SP0

---
+++ SP/0/questions.js
@@ -0,0 +1,9 @@
+questions = [
+    {
+        "question": "What does 'Siempre he querido' mean?",
+        "correctAnswer": "I have always wanted to",
+        "answer0": "I have dreamed of",
+        "answer1": "I really want to",
+        "answer2": "I have to",
+    }
+]
