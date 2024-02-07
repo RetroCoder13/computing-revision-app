d4f61139f5c1c4353b2d03500c87d4b3803179bb
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-02-07 13:40:00 +0000

    Added some questions to PH5

---
+++ PH/5/questions.js
@@ -0,0 +1,30 @@
+questions = [
+    {
+        "question": "Which is a scalar?",
+        "correctAnswer": "Speed",
+        "answer0": "Velocity",
+        "answer1": "Force",
+        "answer2": "Displacement",
+    },
+    {
+        "question": "What does a vector have that a scalar does not?",
+        "correctAnswer": "Direction",
+        "answer0": "Speed",
+        "answer1": "Magnitude",
+        "answer2": "Force",
+    },
+    {
+        "question": "What is the resultant force of 2N up and 5N up?",
+        "correctAnswer": "7N up",
+        "answer0": "7N down",
+        "answer1": "5N left",
+        "answer2": "5N up",
+    },
+    {
+        "question": "What is the resultant force of 3N up and 3N down?",
+        "correctAnswer": "0N",
+        "answer0": "10N up",
+        "answer1": "3N up",
+        "answer2": "10N down",
+    }
+]
