3a884aca1d5a11ad1c00da2fb0597cebf262bb1d
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-05-08 11:13:53 +0000

    Program now calculates the percentage score

--- database.js
+++ database.js
@@ -14,6 +14,8 @@ async function load(){
         document.getElementById('accountUsername').innerHTML = data[0]['username']
         correct = data[0]['correct']
         wrong = data[0]['incorrect']
+        percentageList = data[0]['percentage']
+        document.getElementById('scorePercentage').innerHTML = "Percentage: " + calculatePercentage(data[0]['percentage'])
         if(correct >= 1000){
             
         }
@@ -27,9 +29,17 @@ async function load(){
 }
 
 async function update(){
+    if(prevCorrect < correct){
+        percentageList = createPercentageList(percentageList,1)
+    } else if(prevWrong < wrong){
+        percentageList = createPercentageList(percentageList,0)
+    }
+
+    scorePercentage.innerHTML = "Percentage: " + calculatePercentage(percentageList)
+
     var { data, error } = await supabaseClient
         .from('users')
-        .update({correct:correct, incorrect:wrong})
+        .update({correct:correct, incorrect:wrong, percentage:percentageList})
         .eq('id',JSON.parse(localStorage.getItem("sb-hxfnilmbkorrzhhnohzp-auth-token"))["user"]["id"])
     
     if(username.innerHTML == "Cassian" && prevWrong < wrong){
@@ -64,4 +74,16 @@ async function update(){
 async function signOut(){
     var { error } = await supabaseClient.auth.signOut()
     location.reload()
+}
+
+function createPercentageList(data, value){
+    if(data.length >= 100){
+        data = data.substring(1,data.length)
+    }
+    data += value
+    return data
+}
+
+function calculatePercentage(data){
+    return `${data.split("1").length-1}%`
 }
