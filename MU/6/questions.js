c10306ae0c02153ec861278e691e8c0b36d59455
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-05-09 09:06:03 +0000

    Added questions to MU6

--- MU/6/questions.js
+++ MU/6/questions.js
@@ -5,5 +5,33 @@ questions = [
         "answer0":"Hans Zimmer",
         "answer1":"Alan Silvestri",
         "answer2":"James Newton Howard"
+    },
+    {
+        "question":"How does the music begin?",
+        "correctAnswer":"Brass Fanfare",
+        "answer0":"String Fanfare",
+        "answer1":"Rebel Blockade Runner",
+        "answer2":"Main Theme"
+    },
+    {
+        "question":"What is a leitmotif?",
+        "correctAnswer":"Short melodic idea that represents a character or emotion",
+        "answer0":"The entire score",
+        "answer1":"A type of cadence",
+        "answer2":"A pickup beat before the bar"
+    },
+    {
+        "question":"What is an anacrusis?",
+        "correctAnswer":"A pickup beat before the bar",
+        "answer0":"The entire score",
+        "answer1":"Short melodic idea that represents a character or emotion",
+        "answer2":"A type of cadence"
+    },
+    {
+        "question":"What is the overall structure?",
+        "correctAnswer":"Ternary",
+        "answer0":"Sonata Form",
+        "answer1":"Arch Form",
+        "answer2":"Binary"
     }
 ]
