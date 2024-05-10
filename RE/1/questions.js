3427ab672f9b193fabccf93b65e6997bdf48ee9f
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-05-10 10:54:24 +0000

    Added questions for RE1

---
+++ RE/1/questions.js
@@ -0,0 +1,9 @@
+questions = [
+    {
+        "question": "Which is NOT part of the 'Ten Commandments'",
+        "correctAnswer": "Thou shall not swear",
+        "answer0": "Thou shall not kill",
+        "answer1": "Thou shall not steal ",
+        "answer2": "Thou shall not commit adultery ",
+    }
+]
