eec6e71b63b308b79588863d60d0b97b34b62d5c
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-05-14 12:59:16 +0000

    Added questions for MU5

--- MU/5/questions.js
+++ MU/5/questions.js
@@ -5,5 +5,19 @@ questions = [
         "answer0":"George Gershwin",
         "answer1":"Andrew Lloyd Webber",
         "answer2":"Stephen Sondheim"
+    },
+    {
+        "question":"How does this piece begin?",
+        "correctAnswer":"Spoken Dialogue",
+        "answer0":"Tremolo",
+        "answer1":"Female Singing",
+        "answer2":"Homophonic"
+    },
+    {
+        "question":"What do the strings do when they enter in the first verse?",
+        "correctAnswer":"Tremolo",
+        "answer0":"Glissando",
+        "answer1":"Free Tempo",
+        "answer2":"Dotted Rhythm"
     }
 ]
