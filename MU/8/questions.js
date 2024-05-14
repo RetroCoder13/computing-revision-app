7459b0fd55c24694b9af67f5af845ccaf7f1f01f
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-05-14 12:47:15 +0000

    Added questions to MU8

--- MU/8/questions.js
+++ MU/8/questions.js
@@ -5,5 +5,26 @@ questions = [
         "answer0":"Afro Celt Sound System",
         "answer1":"Simon Emmerson",
         "answer2":"Sinead O'Connor"
+    },
+    {
+        "question":"What type of fusion is this piece?",
+        "correctAnswer":"Bossa Nova",
+        "answer0":"Jazz",
+        "answer1":"African and Celtic",
+        "answer2":"Samba"
+    },
+    {
+        "question":"How does the piece start?",
+        "correctAnswer":"Acoustic Bass Guitar",
+        "answer0":"Double Bass",
+        "answer1":"Fretless Bass",
+        "answer2":"Acoustic Guitar"
+    },
+    {
+        "question":"What does free tempo mean?",
+        "correctAnswer":"No fixed speed",
+        "answer0":"Slow speed",
+        "answer1":"Tempo is set but can be changed if the artist wishes",
+        "answer2":"Fast speed"
     }
 ]
