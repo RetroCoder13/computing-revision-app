f906c3b8dfd88592053b89aef47a8aecf6038ba8
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-05-21 08:48:22 +0000

    Added questions to DT16

---
+++ DT/16/questions.js
@@ -0,0 +1,100 @@
+questions = [
+    {
+        "question":"Which is a hardwood?",
+        "correctAnswer":"Oak",
+        "answer0":"Pine",
+        "answer1":"Red Cedar",
+        "answer2":"Chipboard"
+    },
+    {
+        "question":"Which is a hardwood?",
+        "correctAnswer":"Mahogany",
+        "answer0":"Spruce",
+        "answer1":"Pine",
+        "answer2":"MDF"
+    },
+    {
+        "question":"Which is a hardwood?",
+        "correctAnswer":"Beech",
+        "answer0":"Red Cedar",
+        "answer1":"Plywood",
+        "answer2":"Spruce"
+    },
+    {
+        "question":"Which is a hardwood?",
+        "correctAnswer":"Teak",
+        "answer0":"Chipboard",
+        "answer1":"Blockboard",
+        "answer2":"Pine"
+    },
+    {
+        "question":"Which is a hardwood?",
+        "correctAnswer":"Balsa",
+        "answer0":"Spruce",
+        "answer1":"Plywood",
+        "answer2":"Red Cedar"
+    },
+    {
+        "question":"Which is a softwood?",
+        "correctAnswer":"Pine",
+        "answer0":"Oak",
+        "answer1":"Plywood",
+        "answer2":"Teak"
+    },
+    {
+        "question":"Which is a softwood?",
+        "correctAnswer":"Red Cedar",
+        "answer0":"Balsa",
+        "answer1":"Mahogany",
+        "answer2":"Beech"
+    },
+    {
+        "question":"Which is a softwood?",
+        "correctAnswer":"Spruce",
+        "answer0":"MDF",
+        "answer1":"Teak",
+        "answer2":"Chipboard"
+    },
+    {
+        "question":"Which is a manufactured board?",
+        "correctAnswer":"Plywood",
+        "answer0":"Teak",
+        "answer1":"Spruce",
+        "answer2":"Pine"
+    },
+    {
+        "question":"Which is a manufactured board?",
+        "correctAnswer":"Flexi-Ply",
+        "answer0":"Oak",
+        "answer1":"Mahogany",
+        "answer2":"Red Cedar"
+    },
+    {
+        "question":"Which is a manufactured board?",
+        "correctAnswer":"Marine Ply",
+        "answer0":"Teak",
+        "answer1":"Balsa",
+        "answer2":"Pine"
+    },
+    {
+        "question":"Which is a manufactured board?",
+        "correctAnswer":"MDF",
+        "answer0":"Pine",
+        "answer1":"Balsa",
+        "answer2":"Spruce"
+    },
+    {
+        "question":"Which is a manufactured board?",
+        "correctAnswer":"Blockboard",
+        "answer0":"Mahogany",
+        "answer1":"Teak",
+        "answer2":"Beech"
+    },
+    {
+        "question":"Which is a manufactured board?",
+        "correctAnswer":"Chipboard",
+        "answer0":"Balsa",
+        "answer1":"Oak",
+        "answer2":"Spruce"
+    }
+]
