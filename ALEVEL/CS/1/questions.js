4e715ce774b15a33332d3085cb069218aef1faae
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2024-10-07 13:30:43 +0100

    Added questions

---
+++ ALEVEL/CS/1/questions.js
@@ -0,0 +1,16 @@
+questions = [
+    {
+        "question":"What does MAR stand for?",
+        "correctAnswer":"Memory Address Register",
+        "answer0":"Memory Anchor Register",
+        "answer1":"Memory Architecture Register",
+        "answer2":"Memory Active Register"
+    },
+    {
+        "question":"What does MDR stand for?",
+        "correctAnswer":"Memory Data Register",
+        "answer0":"Memory Date Register",
+        "answer1":"Memory Drone Register",
+        "answer2":"Memory Drain Register"
+    }
+]
